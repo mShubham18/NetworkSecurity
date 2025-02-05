4efd393d90021e301d67caa23c8a565fc0aead9f
mShubham18 <shubhuu1804@gmail.com>
2025-02-05 16:42:40 +0530

    Completed the Training Pipeline part fully and also commited a dry run

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from fastapi.responses import Response
 from uvicorn import run as app_run
 from starlette.responses import RedirectResponse
 import pandas as pd
-
+from networksecurity.components.model_trainer import NetworkModel
 from networksecurity.utils.main_utils.utils import load_object
 
 client = pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
@@ -41,6 +41,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
+
+
+
 @app.get("/",tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -51,6 +56,26 @@ async def train_route():
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
         return Response("Training is Succesful")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+@app.post("/predict")
+
+async def predict_route(request:Request,file:UploadFile=File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor,model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df["predicted_column"]=y_pred
+        print(df["predicted_column"])
+        df.to_csv("prediction_output/output.csv",index=False)
+        table_html = df.to_html(classes="table table-striped")
+        return templates.TemplateResponse("table.html",{"request":request,"table":table_html})
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
 
